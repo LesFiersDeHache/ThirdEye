c4f210c2e4edac06b2a7832f43bf1d4aa7a98559
Lucas Monségu <lucas.mgu@gmail.com>
2016-10-28 16:37:15 +0200

    sigmoid

---
+++ BackFeed.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+float sigmoid(float x)
+{
+	return 1 / (1 + exp(x));
+}
+
+float dSigmoid(float x)
+{
+	float sig = sigmoid(x);
+	return sig * (1 - sig);
+}
