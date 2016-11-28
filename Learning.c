86664540d4c5ec131e69b413e807b463ca2efa93
Maxence CARON--LASNE <maxence.caron-lasne@epita.fr>
2016-11-28 20:20:42 +0100

    Saving

--- Learning.c
+++ Learning.c
@@ -10,9 +10,14 @@
 
 void lrnStartLearning(NeuralNet* NN, char* path, int loops) {
 
+    printf("####################\n");
+    printf("# LEARNING STARTED #\n");
+    printf("####################\n");
     // Init Input/Output
     Mat* Input = mNewFill(24*24, NB_OF_CHAR, 0.0);
     Mat* Output = mNewFill(NB_OF_CHAR, NB_OF_CHAR, 0.0);
+
+    lrnLoadTrainingMatrix(Input, Output);
 
     // Init NeuralNet
     NeuralNet* NN = NnInit(Input, Output, NB_NEURONS_L1, NB_OF_CHAR);
@@ -20,8 +25,12 @@ void lrnStartLearning(NeuralNet* NN, char* path, int loops) {
     // Load weights and bias
     lrnLoadNeuralNet(NN, path);
 
+    warnx("Let's start the leraning !");
+
     // Begin learning
     for ( int l = 0 ; l < loops ; ++l ) {
+
+        warnx("Learning n.%d just began.", l);
 
         // Learn
         NnLearn(NN);
@@ -30,17 +39,28 @@ void lrnStartLearning(NeuralNet* NN, char* path, int loops) {
         lrnSaveNeuralNet(NN, path);
         
         // Print current error 
-        printf("Learning n.%d completed.\n", l);
-        printf("Error : %f\n", NnGetError(NN));
+        warnx("Learning n.%d completed.", l);
+        warnx("Error : %f", NnGetError(NN));
     }
 }
 
 static void lrnSaveNeuralNet(NeuralNet* NN, char* path) {
 
+    warnx("Saving Neural Network...");
     // FIXME
+    warnx("Neural Network Saved.");
 }
 
 static void lrnLoadNeuralNet(NeuralNet* NN, char* path) {
 
-    //FIXME
+    warnx("Loading Neural Network...");
+    // FIXME
+    warnx("Neural Network Loaded.");
+}
+
+static void lrnLoadTrainingMatrix(Mat* Input, Mat* Output) {
+
+    warnx("Loading Training Matrix...");
+    // FIXME
+    warnx("Training Matrix Loaded.");
 }
